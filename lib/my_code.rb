f5bb87fcffa14ade1db591141c19209a748a21e7
Anthony Holder <anthonyh202x@gmail.com>
2020-01-01 17:38:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ end
 
 def reduce_to_total (source_array, starting_point)
 counter = 0 
-#starting_point = 0
+starting_point = 0
 result = starting_point
   while counter < source_array.size
   result += source_array[counter]
