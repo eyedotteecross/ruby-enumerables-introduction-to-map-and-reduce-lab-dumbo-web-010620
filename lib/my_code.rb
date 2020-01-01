6f878353cd8583cf6cd0766a2df5b5f597781a14
Anthony Holder <anthonyh202x@gmail.com>
2020-01-01 17:13:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # My Code here....
+def map_to_negativize (source_array)
+a = 0 
+result = [] 
+  while a < source_array.size 
+  result << source_array[a] * -1
+  a += 1
+  end 
+result
+end
+
+def map_to_no_change (source_array)
+a + 0 
+result = []
+
+
+
+
+end 
